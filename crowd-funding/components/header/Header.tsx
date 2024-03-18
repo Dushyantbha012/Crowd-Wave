0083adf4c63468b190cb7799e7828d1adc2207b0
Dushyant Bhardwaj <dushyantbha012@gmail.com>
2024-03-19 02:11:01 +0530

    styling to landing page and header

--- crowd-funding/components/header/Header.tsx
+++ crowd-funding/components/header/Header.tsx
@@ -1,12 +1,18 @@
+import icon from "./signup.png";
+import Image from "next/image";
 export default function () {
   return (
     <div>
-      <ul className="flex flex-row text-xl justify-between w-screen h-16 border border-black align-middle items-center px-5 font-medium">
+      <ul className="flex flex-row text-xl justify-between w-screen h-16 align-middle items-center px-5 font-medium border-zinc-300 border-b-2">
         <li className="">Crowd Wave</li>
         <li className=" ">
-          <ul className="flex flex-row justify-between gap-10">
-            <li>Sign Up</li>
+          <ul className="flex flex-row justify-between gap-10 items-center align-middle">
+            <li className="flex flex-row align-middle items-center gap-x-2">
+              Sign Up
+              <Image src={icon} width={30} height={30} alt="Signup Icon" />
+            </li>
             <li>Start a Campaign</li>
+            <li>DashBoard</li>
           </ul>
         </li>
       </ul>
